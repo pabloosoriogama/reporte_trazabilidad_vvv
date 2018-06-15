d14f8fe28220781663c546dcd236ee5c4261faf5
pabloosorio <35545995+pabloosorio@users.noreply.github.com>
2018-06-14 19:49:36 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -10,13 +10,16 @@ class StockMoveConteo(models.Model):
     @api.one
     @api.depends('product_id', 'date', 'conteo_stock')
     def _compute_conteo(self):
+    	cr = self.env.cr
     	lista = []
+    	def_pro = self.env.context.get('default_product_id', False)
+    	search_pro = self.env.context.get('search_default_product_id', False)
         for p in self.product_id.product_variant_ids:
         	lista.append(str(p.id))
-    	cr = self.env.cr
+    	
     	total_stock = 0.0
-    	for l in lista:
-	        sql = "select sm.product_uom_qty, sp.name, sl.name from stock_move sm full outer join stock_picking sp on sp.id=sm.picking_id full outer join stock_location sl on sl.id = sm.location_id WHERE sm.product_id='"+str(l)+"' and sm.id<='"+str(self.id)+"' and sm.state='done';"
+    	if(def_pro==search_pro):
+    		sql = "select sm.product_uom_qty, sp.name, sl.name from stock_move sm full outer join stock_picking sp on sp.id=sm.picking_id full outer join stock_location sl on sl.id = sm.location_id WHERE sm.product_id='"+str(def_pro)+"' and sm.id<='"+str(self.id)+"' and sm.state='done';"
 	        cr.execute(str(sql))
 	        filas = cr.fetchall()
 	        if(filas!=[]):
@@ -33,5 +36,24 @@ class StockMoveConteo(models.Model):
 	            			total_stock = total_stock - float(qty)
 	            	if(tt.find("/OUT/")>=0):
 	            		total_stock = total_stock - float(qty)
+    	else:
+	    	for l in lista:
+		        sql = "select sm.product_uom_qty, sp.name, sl.name from stock_move sm full outer join stock_picking sp on sp.id=sm.picking_id full outer join stock_location sl on sl.id = sm.location_id WHERE sm.product_id='"+str(l)+"' and sm.id<='"+str(self.id)+"' and sm.state='done';"
+		        cr.execute(str(sql))
+		        filas = cr.fetchall()
+		        if(filas!=[]):
+		            for c in filas:
+		            	qty = float(c[0])
+		            	tt = str(c[1])
+		            	if(tt.find("/IN/")>=0):
+		            		total_stock = total_stock + float(qty)
+		            	if(tt == 'None'):
+		            		ttt = str(c[2])
+		            		if(ttt.find("adjustment")>=0):
+		            			total_stock = total_stock + float(qty)
+		            		else:
+		            			total_stock = total_stock - float(qty)
+		            	if(tt.find("/OUT/")>=0):
+		            		total_stock = total_stock - float(qty)
 
         self.conteo_stock = total_stock
